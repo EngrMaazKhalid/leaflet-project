abfb9c8cb483fc17a2dc65b749d35a7fbff514a4
EngrMaazKhalid <110035414+EngrMaazKhalid@users.noreply.github.com>
2024-04-14 12:33:23 +0500

    first commit

---
+++ query.js
@@ -0,0 +1,9 @@
+document.querySelector('#hash').addEventListener('click', (e)=>{
+    location.assign('hash.html')
+})
+document.querySelector('#query').addEventListener('click', (e)=>{
+    location.assign('index.html')
+})
+document.querySelector('#co').addEventListener('click', (e)=>{
+    location.assign('lat.html')
+})
